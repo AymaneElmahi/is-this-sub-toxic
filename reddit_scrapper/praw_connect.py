e7b6fda3050760565e3c34a5d20b133cd52d565a
AymaneElmahi <elmahi.aymane@gmail.com>
2024-05-20 15:34:30 +0200

    first version, got 10 comments from subreddit

---
+++ reddit_scrapper/praw_connect.py
@@ -0,0 +1,41 @@
+import praw
+from kafka import KafkaProducer
+from colorama import Fore, Style, init
+import json
+
+# Setup Reddit connection using PRAW with credentials from praw.ini
+reddit = praw.Reddit(
+    site_name='DEFAULT'  # The section name in praw.ini
+)
+
+# Choose the subreddit
+subreddit = reddit.subreddit("EASportsFC")
+
+# Setup Kafka producer
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda m: json.dumps(m).encode('ascii'))
+
+init(autoreset=True)
+
+# Fetch and send a limited number of comments to Kafka
+comment_limit = 10  # Set the number of comments you want to fetch
+for comment in subreddit.stream.comments(pause_after=-1):
+    if comment is None:
+        break
+    message = {'id': comment.id, 'body':comment.body}
+    producer.send('reddit_comments', value=message)
+    producer.flush()
+    print(f"{Fore.GREEN}Sent {Style.RESET_ALL}{comment.body}{Style.RESET_ALL}{Fore.GREEN} "
+          f"to Kafka{Style.RESET_ALL}\n")
+
+    comment_limit -= 1
+    if comment_limit<=0:
+        break
+    
+print(f"{Fore.YELLOW}Remaining requests: "
+          f"{Style.RESET_ALL}{reddit.auth.limits['remaining']}{Fore.YELLOW} "
+          f"out of {Style.RESET_ALL}{reddit.auth.limits['used']}{Fore.YELLOW} "
+          f"used{Style.RESET_ALL}\n")
+
+# Close the producer
+producer.close()
